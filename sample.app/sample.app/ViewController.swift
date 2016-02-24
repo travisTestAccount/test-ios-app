29474d425a774a3429ff33c15ca8292759a4b5bc
Kamil Zielinski <K.Zielinski@kainos.com>
2016-02-24 14:43:41 +0100

    Sample app with 2 test, one failing"

---
+++ sample.app/sample.app/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  sample.app
+//
+//  Created by Kamil Zielinski on 24/02/16.
+//  Copyright © 2016 Kamil Zielinski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        NSLog("Hello world")
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
